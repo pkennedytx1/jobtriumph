2963b779adbe0c568dcc16fe482650f0142b91a8
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-06-08 18:56:04 -0500

    added navbar

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return(
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="" width="30" height="30" class="d-inline-block align-top" alt="" />
+                    Job Flow
+                </a>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Log a Job</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+export default Navbar;
