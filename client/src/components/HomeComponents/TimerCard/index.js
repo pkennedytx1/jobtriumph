dcf51b22258637f463997fef99ea75891825cb86
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-06-13 09:31:26 -0400

    timer functionality added

--- client/src/components/HomeComponents/TimerCard/index.js
+++ client/src/components/HomeComponents/TimerCard/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import ms from 'pretty-ms';
 
 class TimerCard extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          time: 0,
+          isOn: false,
+          start: 0
+        }
+        this.startTimer = this.startTimer.bind(this)
+        this.stopTimer = this.stopTimer.bind(this)
+        this.resetTimer = this.resetTimer.bind(this)
+    }
+
+    startTimer() {
+        this.setState({
+          isOn: true,
+          time: this.state.time,
+          start: Date.now() - this.state.time
+        })
+        this.timer = setInterval(() => this.setState({
+          time: Date.now() - this.state.start
+        }), 1);
+    }
+    stopTimer() {
+        this.setState({isOn: false})
+        clearInterval(this.timer)
+    }
+    resetTimer() {
+        this.setState({time: 0, isOn: false})
+    }
+
     render() {
+
+        let start = (this.state.time == 0) ? <button onClick={this.startTimer}>start</button> : null
+        let stop = (this.state.time == 0 || !this.state.isOn) ? null : <button onClick={this.stopTimer}>stop</button>
+        let resume = (this.state.time == 0 || this.state.isOn) ? null : <button onClick={this.startTimer}>resume</button>
+        let reset = (this.state.time == 0 || this.state.isOn) ? null : <button onClick={this.resetTimer}>reset</button>
+
         return(
             <div>
                 <div class="card">
@@ -9,8 +46,11 @@ class TimerCard extends React.Component {
                     <b>Work</b>Timer
                 </div>
                 <div class="card-body">
-                    <h1 class="card-title">00:00</h1>
-                    <a href="#" class="btn btn-primary">Start Working!</a>
+                    <h1 class="card-title">{this.state.time}</h1>
+                    {start}
+                    {resume}
+                    {stop}
+                    {reset}
                     <p class="card-text">Total Worktime and breaktime</p>
                 </div>
                 </div>
